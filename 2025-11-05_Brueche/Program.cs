6377bdb729e577ed78ccb3a09c6e699232b092e9
silv-salma <KHA230102@spengergasse>
2025-11-05 10:34:19 +0100

    dotnet_brueche

---
+++ 2025-11-05_Brueche/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World als Klasse!");
+        foreach (string s in args)
+        {
+            Console.WriteLine(s);
+        }
+
+}
+}
+
