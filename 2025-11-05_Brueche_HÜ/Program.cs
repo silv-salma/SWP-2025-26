0c8b1ec07ea35e8680dbc8e33f3cb6f4dd3ad48f
silv-salma <KHA230102@spengergasse>
2025-11-18 23:19:44 +0100

    BruchTests_erweitern

--- 2025-11-05_Brueche_HÜ/Program.cs
+++ 2025-11-05_Brueche_HÜ/Program.cs
@@ -1,21 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Collections.Specialized;
-
+using System.Linq.Expressions;
+using System;
 class Program
 {
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World, als Klasse!(blub blub blub)");
-        foreach (string s in args)
+        if (args.Length < 2)
         {
-            Console.WriteLine(s);
+            Console.WriteLine("Bitte mindestens zwei Brüche angeben (z.B. \"1/2\" \"3/4\").");
+            return;
         }
-        Bruch b1 = new Bruch(args[0]);
-        Bruch b2 = new Bruch(args[1]);
-        Bruch b3 = b1.addiere(b2);
-        Console.WriteLine("Ergebnis: " + b3.ToString());
 
+        try
+        {
+            Bruch b1 = new Bruch(args[0]);
+            Bruch b2 = new Bruch(args[1]);
+            Bruch b3 = b1.addiere(b2);
+            Console.WriteLine("Ergebnis: " + b3.ToString());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Fehler bei der Verarbeitung: " + ex.Message);
+            Console.WriteLine("Bitte geben Sie die Brüche im Format 'Zähler/Nenner' ein, z.B. 2/5 oder 3/4.");
+        }
     }
 }
